caf0f0d7b9ce04de236214a6f68a04804dd8faea
Anix <anik220798@gmail.com>
2020-03-07 21:26:28 +0000

    Chore: keeping leading and trailing comments in => fn

--- tests/lib/rules/arrow-parens.js
+++ tests/lib/rules/arrow-parens.js
@@ -68,7 +68,11 @@ const valid = [
     { code: "async a => ({})", options: ["as-needed", { requireForBlockBody: true }], parserOptions: { ecmaVersion: 8 } },
     { code: "async a => a", options: ["as-needed", { requireForBlockBody: true }], parserOptions: { ecmaVersion: 8 } },
     { code: "(a: T) => a", options: ["as-needed", { requireForBlockBody: true }], parser: parser("identifer-type") },
-    { code: "(a): T => a", options: ["as-needed", { requireForBlockBody: true }], parser: parser("return-type") }
+    { code: "(a): T => a", options: ["as-needed", { requireForBlockBody: true }], parser: parser("return-type") },
+
+
+    // "as-needed", { "requireCommentsParams": true }
+    { code: "const f = (/** @type {number} */a) => a + a;", options: ["as-needed", { requireCommentsParams: true }] }
 ];
 
 const type = "ArrowFunctionExpression";
